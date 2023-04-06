f7322d4cd816445f84f1ac715a7b46e6459812b3
Omar <77851226+omaryahia4@users.noreply.github.com>
2023-04-06 09:24:35 +0100

    Update markdown2html.py

--- markdown2html.py
+++ markdown2html.py
@@ -9,47 +9,31 @@ but formatted as HTML.
 It supports headings, unordered lists, and paragraphs.
 """
 
-from sys import argv, stderr
-from os.path import exists
+from sys import stderr, argv
+import os
+import markdown
 
-if __name__ == "__main__":
+
+if __name__ == '__main__':
 
     if len(argv) < 3:
         print("Usage: ./markdown2html.py README.md README.html", file=stderr)
         exit(1)
-
-    if not exists(argv[1]):
-        print(f"Missing {argv[1]}", file=stderr)
+    elif os.path.exists(argv[1]) is False:
+        print("Missing " + argv[1], file=stderr)
         exit(1)
+    else:
+        # Open input and output files
+        src_file = open(argv[1], "r")
+        dest_file = open(argv[2], "w")
 
-    with open(argv[1], 'r', encoding='utf-8') as file:
-        with open(argv[2], "w", encoding='utf-8') as html:
-            nextLine = file.readline()
-            while nextLine:
-                nbhash = nextLine.count("#")
-                unorderList = nextLine.strip("- ").rstrip()
-                orderedList = nextLine.strip("* ").rstrip()
-                if nbhash != 0:
-                    line = nextLine.lstrip('# ').lstrip('/n')
-                    html.write(f'<h{nbhash}>{line.rstrip()}</h{nbhash}>\n')
-                if '-' in nextLine:
-                    html.write('<ul>\n')
-                    html.write(f'<li>{unorderList}</li>\n')
-                    file.readline()
-                    while nextLine.startswith('-'):
-                        html.write(f'<li>{nextLine.strip("- ").rstrip()}</'
-                                   f'li>\n')
-                        nextLine = file.readline()
-                    html.write('</ul>\n')
-                if '*' in nextLine:
-                    html.write('<ol>\n')
-                    html.write(f'<li>{orderedList}</li>\n')
-                    file.readline()
-                    while nextLine.startswith('*'):
-                        html.write(f'<li>{nextLine.strip("* ").rstrip()}</'
-                                   f'li>\n')
-                        nextLine = file.readline()
-                    html.write('</ol>\n')
-                else:
-                    nextLine = file.readline()
+        # Convert Markdown to HTML
+        hmtl = markdown.markdown(src_file.read())
+
+        # Write HTML content to output file
+        dest_file.write(hmtl + '\n')
+
+        # Close files and exit
+        src_file.close()
+        dest_file.close()
         exit(0)
