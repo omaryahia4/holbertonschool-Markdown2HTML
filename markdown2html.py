647e84f4f8d945ea7c1fcc550e092eb2e95b790c
omaryahia4 <omar.yahia4444@gmail.com>
2023-08-01 14:48:04 +0100

    test

--- markdown2html.py
+++ markdown2html.py
@@ -1,31 +1,28 @@
 #!/usr/bin/python3
-"""
-markdown2html.py - Convert a markdown file to HTML format.
+"""some script to start"""
 
-Usage: ./markdown2html.py input.md output.html
-
-This script reads a markdown file and writes an HTML file with the same content
-but formatted as HTML.
-It supports headings, unordered lists, and paragraphs.
-"""
-
-from sys import stderr, argv
+import sys
 import os
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
+    if len(sys.argv) < 3:
+        sys.stderr.write("Usage: ./markdown2html.py README.md README.html\n")
+        sys.exit(1)
 
-    if len(argv) < 3:
-        print("Usage: ./markdown2html.py README.md README.html", file=stderr)
-        exit(1)
-    elif os.path.exists(argv[1]) is False:
-        print("Missing " + argv[1], file=stderr)
-        exit(1)
-    else:
-        src_file = open(argv[1], "r")
-        dest_file = open(argv[2], "w")
-        hmtl = markdown.markdown(src_file.read())
-        dest_file.write(hmtl + '\n')
-        src_file.close()
-        dest_file.close()
-        exit(0)
+    if not os.path.isfile(sys.argv[1]):
+        sys.stderr.write(f"Missing {sys.argv[1]}\n")
+        sys.exit(1)
+
+    with open(sys.argv[1], 'r') as file:
+        lines = file.readlines()
+    with open(sys.argv[1], 'w') as file:
+        for line in lines:
+            line = line.strip()
+            if line.startswith("#"):
+                level = line.count("#")
+                text = line.strip("#").strip()
+                html = f"<h{level}>text</h{level}>\n"
+                file.write(html)
+            else:
+                file.write(f"{line}\n")
